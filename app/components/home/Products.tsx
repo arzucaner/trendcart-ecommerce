1dd5ba606ff4d3225670da7605c0f05d757107c1
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-13 22:13:48 +0000

    Create Heading.tsx

---
+++ app/components/home/Products.tsx
@@ -0,0 +1,11 @@
+import Heading from "../general/Heading";
+
+const Products = () => {
+    return (
+        <div>
+            <Heading text="All Products" />
+        </div>
+    )
+}
+
+export default Products;
