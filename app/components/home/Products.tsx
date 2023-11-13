34258c032ae4d1d71b084079ed67c633984f6f23
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-13 22:45:27 +0000

    Create product.tsx

--- app/components/home/Products.tsx
+++ app/components/home/Products.tsx
@@ -1,9 +1,18 @@
+import { products } from "@/utils/Products";
 import Heading from "../general/Heading";
+import ProductCard from "./productCard";
 
 const Products = () => {
     return (
         <div>
             <Heading text="All Products" />
+            <div>
+                {
+                    products.map(product => (
+                        <ProductCard key={product.id} product={product}/>
+                    ))
+                }
+            </div>
         </div>
     )
 }
