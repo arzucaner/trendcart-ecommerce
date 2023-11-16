ec8cce9c56f390f8e1e6ecfe54252e85b69d3e78
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-16 22:59:26 +0000

    Update productcard.tsx

--- app/components/home/ProductCard.tsx
+++ app/components/home/ProductCard.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 const ProductCard = ({ product }: { product: any }) => {
     return (
-        <div className="w-[240px] shadow-lg p-2">
-            <div className="relative">
+        <div className="w-[240px] shadow-lg p-2 rounded-md">
+            <div className="relative h-[150px]">
                 <Image src={product.image} fill alt="" className="object-contain" />
 
+            </div>
+            <div className="text-center mt-2">
+                <div>{product.name}</div>
             </div>
 
         </div>
