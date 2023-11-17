669fb03fcab7c1ddaf4e118e1b930d1b8dd30968
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-17 23:37:41 +0000

    Update detailclient.tsx

--- app/components/detail/DetailClient.tsx
+++ app/components/detail/DetailClient.tsx
@@ -8,11 +8,14 @@ const DetailClient = ({ product }: { product: any }) => {
     return (
         <div className="my-10">
             <PageContainer>
-                <div className="flex">
-                    <div className="relative h-[400px] w-[200px] flex-1">
+                <div className="block md:flex gap-10 justify-center">
+                    <div className="relative h-[400px] w-[400px]">
                         <Image src={product?.image} fill alt="" />
                     </div>
-                    <div className="w-3/4">right</div>
+                    <div className="w-1/2 space-y-3">
+                        <div className="text-xl md:text-2xl">{product?.name}</div>
+                        <div className="text-slate-500">{product?.description}</div>
+                    </div>
                 </div>
             </PageContainer>
         </div>
