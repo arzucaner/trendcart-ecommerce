43bac1ca27676354958b8990bccb25a3c4960e41
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-17 23:26:48 +0000

    Update detailclient.tsx

--- app/components/detail/DetailClient.tsx
+++ app/components/detail/DetailClient.tsx
@@ -1,15 +1,20 @@
 "use client"
 
 import PageContainer from "../containers/PageContainer";
+import Image from "next/image";
 
 const DetailClient = ({ product }: { product: any }) => {
 
     return (
-        <div>
+        <div className="my-10">
             <PageContainer>
-                {product?.name}
+                <div className="flex">
+                    <div className="relative h-[400px] w-[200px] flex-1">
+                        <Image src={product?.image} fill alt="" />
+                    </div>
+                    <div className="w-3/4">right</div>
+                </div>
             </PageContainer>
-
         </div>
     )
 }
