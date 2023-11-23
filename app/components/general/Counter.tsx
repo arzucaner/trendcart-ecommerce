d2c6ba8716990e54a1ac4cd7c39eaf88866d0713
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-23 21:41:13 +0000

    Add counter props

--- app/components/general/Counter.tsx
+++ app/components/general/Counter.tsx
@@ -1,14 +1,16 @@
+import { CardProductProps } from "../detail/DetailClient";
+
 interface CounterProps {
-    productCard : any
-    incraseFunc: () => void;
-    decraseFunc: () => void;
+  cardProduct: CardProductProps,
+  incraseFunc: () => void;
+  decraseFunc: () => void;
 }
-const Counter:React.FC<CounterProps> = ({cardProduct, incraseFunc, decraseFunc }) => {
+const Counter: React.FC<CounterProps> = ({ cardProduct, incraseFunc, decraseFunc }) => {
   return (
     <div>
-      <div>-</div>
-      <div>0</div>
-      <div>+</div>
+      <div onClick={decraseFunc}>-</div>
+      <div>{cardProduct?.quantity}</div>
+      <div onClick={incraseFunc}>+</div>
     </div>
   )
 }
