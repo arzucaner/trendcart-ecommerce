4f7eab37c5cc6362bd251d4eedb86c691bebfdaf
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-24 21:21:02 +0000

    Update comment.tsx

--- app/components/detail/Comment.tsx
+++ app/components/detail/Comment.tsx
@@ -1,11 +1,15 @@
+"use client"
 
+import Avatar from "../general/Avatar";
 
-const Comment = ({prd}: {prd : any}) => {
+const Comment = ({ prd }: { prd: any }) => {
 
     console.log(prd, "prd")
-  return (
-    <div>Comment</div>
-  )
+    return (
+        <div>
+            <Avatar image={prd?.user?.image} />
+        </div>
+    )
 }
 
 export default Comment
