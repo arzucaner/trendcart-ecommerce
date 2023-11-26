0e8d350d71ecfae85d44a716a54ed7e14514adad
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-26 23:18:27 +0000

    Update  Cartclient.tsx

--- app/components/cart/CartClient.tsx
+++ app/components/cart/CartClient.tsx
@@ -1,9 +1,22 @@
 "use  Client"
 
+import UseCart from "@/hooks/UseCart"
+import PageContainer from "../containers/PageContainer"
+
 const CartClient = () => {
+    const {cartPrdcts} = UseCart()
+
+    console.log(cartPrdcts, "cartPrdcts")
+    if(!cartPrdcts || cartPrdcts.length === 0){
+        return <div>Your basket is empty...</div>
+    }
   return (
     <div>
-      
+      <PageContainer>
+        <div>
+
+        </div>
+       </PageContainer>
     </div>
   )
 }
