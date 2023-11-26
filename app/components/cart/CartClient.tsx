6f393f9d444cc1906617602bba6042e57e1c6e3d
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-26 23:25:50 +0000

    Add product informations

--- app/components/cart/CartClient.tsx
+++ app/components/cart/CartClient.tsx
@@ -4,21 +4,24 @@ import UseCart from "@/hooks/UseCart"
 import PageContainer from "../containers/PageContainer"
 
 const CartClient = () => {
-    const {cartPrdcts} = UseCart()
+    const { cartPrdcts } = UseCart()
 
     console.log(cartPrdcts, "cartPrdcts")
-    if(!cartPrdcts || cartPrdcts.length === 0){
+    if (!cartPrdcts || cartPrdcts.length === 0) {
         return <div>Your basket is empty...</div>
     }
-  return (
-    <div>
-      <PageContainer>
+    return (
         <div>
-
+            <PageContainer>
+                <div className="flex items-center gap-3 justify-between">
+                    <div>Product Image</div>
+                    <div>Product Name</div>
+                    <div>Product Quantity</div>
+                    <div>Product Price</div>
+                </div>
+            </PageContainer>
         </div>
-       </PageContainer>
-    </div>
-  )
+    )
 }
 
 export default CartClient
