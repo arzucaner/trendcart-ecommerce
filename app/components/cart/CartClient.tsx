ef7ebb46a594c92312fa3a3ba8c9988944ae20b4
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-28 22:54:22 +0000

    Update cardcount.tsk

--- app/components/cart/CartClient.tsx
+++ app/components/cart/CartClient.tsx
@@ -2,6 +2,7 @@
 
 import UseCart from "@/hooks/UseCart";
 import PageContainer from "../containers/PageContainer";
+import Image from "next/image";
 
 
 const CartClient = () => {
@@ -14,22 +15,26 @@ const CartClient = () => {
     return (
         <div className="my-3 md:my-10">
             <PageContainer>
-                <div className="flex items-center gap-3 justify-between">
-                    <div>Product Image</div>
-                    <div>Product Name</div>
-                    <div>Product Quantity</div>
-                    <div>Product Price</div>
+                <div className="flex items-center gap-3 text-center border-b py-3">
+                    <div className="w-1/5">Product Image</div>
+                    <div className="w-1/5">Product Name</div>
+                    <div className="w-1/5">Product Quantity</div>
+                    <div className="w-1/5">Product Price</div>
+                    <div className="w-1/5"></div>               
                 </div>
                 <div>
                     {
                         cartPrdcts.map(cart => (
-                            <div key={cart.id}>
-                                <div>
+                            <div className="flex items-center justify-between text-center my-5" key={cart.id}>
+                                <div className="w-1/4 flex items-center justify-center">
                                     <Image src={cart.image} width={40} height={40} alt="" />
                                 </div>
-                                <div>{cart.name}</div>
-                                <div>2</div>
-                                <div>{cart.price} £</div>
+                                <div className="w-1/5">{cart.name}</div>
+                                <div className="w-1/5">2</div>
+                                <div className="w-1/5 text-orange-600 text-lg">{cart.price} £</div>
+                                 <div className="w-1/5">
+                                    <Button text="Remove Product" small onClick={() => {}}/>
+                                    </div>
                             </div>
                         ))
                     }
--- app/components/navbar/CardCount.tsx
+++ app/components/navbar/CardCount.tsx
@@ -1,7 +1,10 @@
-
+"use client"
+import { IoMdBasket } from "react-icons/io";
 const CardCount = () => {
     return (
-        <div className="hidden md:flex">CardCount</div>
+        <div className="hidden md:flex">
+            <IoMdBasket size="25" />
+        </div>
     )
 }
 
