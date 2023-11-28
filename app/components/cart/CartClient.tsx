1c394bd052eee0829e7a9119af733b228ab069e2
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-28 22:14:45 +0000

    Update cartclient.tsk

--- app/components/cart/CartClient.tsx
+++ app/components/cart/CartClient.tsx
@@ -1,7 +1,8 @@
 "use  Client"
 
-import UseCart from "@/hooks/UseCart"
-import PageContainer from "../containers/PageContainer"
+import UseCart from "@/hooks/UseCart";
+import PageContainer from "../containers/PageContainer";
+
 
 const CartClient = () => {
     const { cartPrdcts } = UseCart()
@@ -11,13 +12,27 @@ const CartClient = () => {
         return <div>Your basket is empty...</div>
     }
     return (
-        <div>
+        <div className="my-3 md:my-10">
             <PageContainer>
                 <div className="flex items-center gap-3 justify-between">
                     <div>Product Image</div>
                     <div>Product Name</div>
                     <div>Product Quantity</div>
                     <div>Product Price</div>
+                </div>
+                <div>
+                    {
+                        cartPrdcts.map(cart => (
+                            <div key={cart.id}>
+                                <div>
+                                    <Image src={cart.image} width={40} height={40} alt="" />
+                                </div>
+                                <div>{cart.name}</div>
+                                <div>2</div>
+                                <div>{cart.price} £</div>
+                            </div>
+                        ))
+                    }
                 </div>
             </PageContainer>
         </div>
