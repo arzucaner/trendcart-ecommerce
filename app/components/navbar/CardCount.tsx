31133a144d826dced4ea3408d04fa3866b79be20
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-28 23:38:37 +0000

    Add basket functionality and define CSS styles

--- app/components/navbar/CardCount.tsx
+++ app/components/navbar/CardCount.tsx
@@ -1,9 +1,12 @@
 "use client"
 import { IoMdBasket } from "react-icons/io";
+import UseCart from '@/hooks/UseCart'
 const CardCount = () => {
+    const { cartPrdcts } = UseCart()
     return (
-        <div className="hidden md:flex">
-            <IoMdBasket size="25" />
+        <div className="hidden md:flex relative">
+            <IoMdBasket size="30" />
+            <div className='absolyte -top-1 right-2 text-xs bg-orange-900 w-5 h-5 flex items-center justify-center rounded-full'>{cartPrdcts?.length}</div>
         </div>
     )
 }
